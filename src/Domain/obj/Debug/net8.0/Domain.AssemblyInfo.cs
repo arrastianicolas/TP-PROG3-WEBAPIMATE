32519229adddc6e49a5b7a0d6ef721a9c858538e
SantiagoHaquin <santihaquin321@gmail.com>
2024-06-24 22:53:22 -0300

    Fixes

--- src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881193680d038191d7fb7c31779713cb7d2a50bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a12cc27a2c32fefc2ad3de9fc5c4356ee5b94a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
